999b1c5bdc4de2e23ff7847d39cb7787a1274342
Sherwin Moodley <213538912@stu.ukzn.ac.za>
2016-03-05 19:29:40 +0200

    dice main code added, dice average function added, destructor and private class variable removed

--- Tut3-Dice(main).cpp
+++ Tut3-Dice(main).cpp
@@ -8,39 +8,51 @@ class Dice
 {
 	public:
 		Dice(void);
-		~Dice(void);
 		int roll(void);
-		int getrollcounter(void);
-	
-	private:
-		int rollcount;
 };
 
 Dice::Dice()	//contructor.
 {
-	rollcount = 0;
 	srand(time(NULL));	//seeding of random number generator.
-}
-
-Dice::~Dice()	//destructor.
-{
-
-}
-
-int Dice::getrollcounter()
-{
-	return rollcount;
 }
 
 int Dice::roll()
 {
-	rollcount++;
 	return (rand() % 6) + 1; // generates random numbers from 1 - 6, increments rollcount each time.
+}
+
+float average(Dice dice, int num)
+{
+	int i;
+	float r;
+	float c = 0;
+
+	for (i = 1; i <= num; i++)
+	{
+		r = dice.roll();
+		c = c + r;
+		cout << "Roll " << i << ": " << r << endl;
+	}
+	
+	
+	return (c / (float)num);
 }
 
 int main()
 {
+	int i, n;
 	Dice dice;
-	cout << dice.roll() << endl;
 
+	cout << "Enter how many times you wish to roll the dice:";
+	cin >> n;
+	
+	cout.precision(4);
+	cout <<"The average value rolled out of "<< n << " roll(s) is: "<< fixed << average(dice, n) << endl;
+
+
+
+	return 0;
 }
+
+
+		
